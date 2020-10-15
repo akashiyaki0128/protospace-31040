81eb7540690cf9f6f9798036cdbf496043c49fd7
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-10-15 21:31:09 +0900

    画像のアップロード機能実装
    １，mini_magickとimage_processingのgemインストール　概要追記
    ２，rails active_storage:install
    ３，マイグレーション
    ４，モデルにhas_one_attached　アソシエーション追記

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,31 @@
 class Prototype < ApplicationRecord
 
   belongs_to :user
+  has_one_attached :image
 end
+
+# Active Storageでファイルをアップロードして保存した後、画像を加工してから（サイズや色など）表示できるように実装します。
+
+# 画像加工のために必要なImageMagickという画像変換ツールと、それをRailsから使うためのGemをインストールしましょう。
+
+#  ImageMagick
+# コマンドラインから画像に処理を加えることができるツールです。
+# 処理としては、画像の作成やサイズ変更、保存形式の変更などがあります。
+
+# ImageMagickはGemではなく、ソフトウェアです。
+# そのため、Homebrewからインストールします。
+
+# GemではないImageMagickをRubyやRailsで扱うには、MiniMagickというGemが必要となります。
+
+#  MiniMagick
+# ImageMagickの機能をRubyで扱えるようにしてくれるGemです。
+# RailsでImageMagickを扱うために必要となります。
+
+# MiniMagickによって、ImageMagickの機能がRailsで使用できるようになりますが、画像サイズの変更には、もう1つImageProcessingというGemを追加する必要があります。
+
+#  ImageProcessing
+# MiniMagickでは提供できない、画像サイズを調整する機能を提供するGemです。
+
+# たくさんのツールとGemを紹介しましたが、それぞれの関係性は把握しておきましょう。
+# それでは、これらのツールとGemを導入します。
+
